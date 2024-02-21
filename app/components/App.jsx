16487bc858150336dc1f251bc2af3dfd40ea8a4c
jdev08 <92550746+jdev082@users.noreply.github.com>
2024-02-21 17:51:47 -0600

    Initial commit

---
+++ app/components/App.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var React = require('react');
+
+/**
+ * App component.
+ */
+module.exports = class App extends React.Component {
+    render() {
+        return (
+            <h1>
+                Welcome to electron-react-template!
+            </h1>
+        );
+    }
+}
